9d8eaec0958b986d493d89a335d4c5d87beaf2c0
Agustin Aused <agus.aused@gmail.com>
2025-05-22 01:05:31 -0300

    Add cache control headers to axios requests in authService

--- src/services/authService.js
+++ src/services/authService.js
@@ -28,6 +28,9 @@ const authService = {
         headers: {
           'Content-Type': 'application/json',
           'Accept': 'application/json',
+          'Cache-Control': 'no-cache, no-store, must-revalidate',
+          'Pragma': 'no-cache',
+          'Expires': '0'
         }
       });
       
